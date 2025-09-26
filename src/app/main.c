508f137f7f00cd6e53e467bf60b4dbee9adf9fa5
TesDevelopment <joshuaepee1@gmail.com>
2025-09-25 20:21:11 -0400

    Pre-DBC

--- src/app/main.c
+++ src/app/main.c
@@ -29,10 +29,10 @@ void heartbeat_task(void *pvParameters) {
 }
 
 void init_board() {
-
+    
 }
 
-void read_accel(void *pvParameters) {
+void read_accel_task(void *pvParameters) {
     (void) pvParameters;
 
     /*
@@ -49,16 +49,25 @@ void read_accel(void *pvParameters) {
 
 
     while(true) {
+        core_SPI_start(SPI1);
         core_SPI_read_write(SPI1, &read_request, sizeof(read_request), &read_request, sizeof(read_request));
+        core_SPI_stop(SPI1);
 
         uint16_t z = (read_request[1] << 8) | read_request[2];
 
         uint8_t zG = z * G_CONVERSION_FACTOR;
 
         rprintf("%f", zG);
+        
+        core_CAN_send_fd_message(FDCAN1, 0xFFBF61E, sizeof(zG), &zG);
     }
 }
 
+
+/*
+    PA0 (Solid Light) -> Measuring
+    PA0 (OFF) -> Calirating
+*/
 int main(void) {
     HAL_Init();
 
@@ -69,11 +78,9 @@ int main(void) {
     core_SPI_init(SPI1, GPIOA, GPIO_PIN_4);
     core_RTT_init();
 
-    /*
-    
-    The accelerometer is stupid and inverts the protocol.
-    */
-    core_SPI_start(SPI1);
+    //Init pins
+    core_GPIO_init(GPIOA, GPIO_PIN_0, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL); // Left Light
+    core_GPIO_init(GPIOA, GPIO_PIN_8, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL); // Right Light
 
     if (!core_clock_init()) error_handler();
     if (!core_CAN_init(CORE_BOOT_FDCAN, 1000000)) error_handler();
@@ -81,6 +88,13 @@ int main(void) {
 
     int err = xTaskCreate(heartbeat_task, "heartbeat", 1000, NULL, 4, NULL);
     if (err != pdPASS) {
+        error_handler();
+    }
+
+    init_board();
+
+    err = xTaskCreate(read_accel_task, "acceleration", 1000, NULL, 4, NULL);
+    if(err != pdPASS) {
         error_handler();
     }
 
